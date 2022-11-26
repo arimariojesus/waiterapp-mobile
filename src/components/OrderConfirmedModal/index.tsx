ee5e113daee25c195b9ff70f0da6122b3718c39e
arimariojesus <arimario.jesus@hotmail.com>
2022-11-26 16:11:35 -0300

    feat: add OrderConfirmedModal component

---
+++ src/components/OrderConfirmedModal/index.tsx
@@ -0,0 +1,37 @@
+import { Modal } from 'react-native';
+
+import { CheckCircle } from '../Icons/CheckCircle';
+import { Text } from '../Text';
+
+import * as S from './styles';
+
+interface OrderConfirmedModalProps {
+  visible: boolean;
+  onConfirm: () => void;
+}
+
+export const OrderConfirmedModal = ({ visible, onConfirm }: OrderConfirmedModalProps) => {
+  return (
+    <Modal
+      visible={visible}
+      animationType="fade"
+    >
+      <S.Container>
+        <CheckCircle />
+
+        <Text size={20} weight="600" color="#fff" style={{ marginTop: 12 }}>
+          Pedido confirmado
+        </Text>
+        <Text color="#fff" opacity={0.8} style={{ marginTop: 4 }}>
+          O pedido já entrou na fila de produção
+        </Text>
+
+        <S.ConfirmButton onPress={onConfirm}>
+          <Text weight="600" color="#d73035">
+            OK
+          </Text>
+        </S.ConfirmButton>
+      </S.Container>
+    </Modal>
+  );
+};
