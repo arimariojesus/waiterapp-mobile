06bf200836d90d581449d9df530643e5b0350592
arimariojesus <arimario.jesus@hotmail.com>
2022-11-26 12:56:58 -0300

    feat: add Menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,63 @@
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+
+import { CheckCircle } from '../Icons/CheckCircle';
+import { ProductModal } from '../ProductModal';
+import { Text } from '../Text';
+import { products } from '../../mocks/products';
+import { IProduct } from '../../types/product';
+import { formatCurrency } from '../../utils';
+
+import * as S from './styles';
+
+export const Menu = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<IProduct | null>(null);
+
+  const handleOpenModal = (product: IProduct) => {
+    setSelectedProduct(product);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      <ProductModal
+        visible={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        product={selectedProduct}
+      />
+
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        ItemSeparatorComponent={S.Separator}
+        keyExtractor={product => product._id}
+        renderItem={({ item: product }) => (
+          <S.Product onPress={() => handleOpenModal(product)}>
+            <S.ProductImage
+              resizeMode="cover"
+              source={{
+                uri: `http://10.0.2.2:3333/uploads/${product.imagePath}`,
+              }}
+            />
+
+            <S.ProductDetails>
+              <Text weight="600">{product.name}</Text>
+              <Text size={14} color="#664" style={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text size={14} weight="600">
+                {formatCurrency(product.price)}
+              </Text>
+            </S.ProductDetails>
+
+            <S.AddToCartButton>
+              <CheckCircle />
+            </S.AddToCartButton>
+          </S.Product>
+        )}
+      />
+    </>
+  );
+};
