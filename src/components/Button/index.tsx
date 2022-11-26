8f898902965b3309865c2c976167a36bfc0bb45f
arimariojesus <arimario.jesus@hotmail.com>
2022-11-26 16:18:58 -0300

    feat(Button): add loading state

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,25 @@
+import { ActivityIndicator } from 'react-native';
 import { Text } from '../Text';
+
 import * as S from './styles';
 
 interface ButtonProps {
   children: string;
   onPress?: () => void;
   disabled?: boolean;
+  isLoading?: boolean;
 }
 
-export const Button = ({ children, onPress, disabled }: ButtonProps) => {
+export const Button = ({ children, onPress, disabled, isLoading }: ButtonProps) => {
   return (
-    <S.Container onPress={onPress} disabled={disabled}>
-      <Text weight="600" color="#fff">{children}</Text>
+    <S.Container onPress={onPress} disabled={disabled || isLoading}>
+      <Text weight="600" color="#fff" opacity={isLoading ? 0 : 1}>
+        {children}
+      </Text>
+
+      {isLoading && (
+        <ActivityIndicator color="#fff" style={{ position: 'absolute' }} />
+      )}
     </S.Container>
   );
 };
