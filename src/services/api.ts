ff7b82d9d816b3cdd9c2adf426ec67e422d110b7
arimariojesus <arimario.jesus@hotmail.com>
2022-12-03 11:04:10 -0300

    feat: export baseURL from api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 import { isAndroid } from '../constants';
 
-const baseURL = isAndroid ? 'http://10.0.2.2:3333' : 'http://localhost:3333';
+export const baseURL = isAndroid
+  ? 'http://10.0.2.2:3333'
+  : 'http://localhost:3333';
 
 export const api = axios.create({
   baseURL,
