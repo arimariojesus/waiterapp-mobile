a6d5b394da7eb79cc01c26255d292adf01d89d9b
arimariojesus <arimario.jesus@hotmail.com>
2022-12-08 20:25:54 -0300

    chore: set styled theme type + set useInterfaceStyle to automatic

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,12 @@
+import 'styled-components';
+
+import theme from '../styles/theme';
+
+export type ThemeMode = 'light' | 'dark';
+export type Theme = typeof theme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends Omit<Theme, 'colors'> {
+    mode?: ThemeMode;
+  }
+}
